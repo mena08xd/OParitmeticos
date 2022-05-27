c3b1e203f45850aa0d098a021f6e5c6c33b0f06a
Ximena Valenzuela <xduarte@uabc.edu.mx>
2022-05-27 01:13:26 -0700

    Agregar archivos de proyecto.

---
+++ OParitmeticos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OParitmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 8;
+            double num2 = 4;
+            double resultado;
+
+            String saludo = "Hola";
+            String Nombre = "Bro";
+
+            resultado = num1 + 22;
+            //Console.WriteLine(resultado);
+            Console.WriteLine(saludo + " " + Nombre + " " + "Como estas?");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OParitmeticos/multi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OParitmeticos
+{
+    class multi
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 5;
+
+            Console.WriteLine(num1 * num2);
+            Console.ReadKey();
+        }
+    }
+}
